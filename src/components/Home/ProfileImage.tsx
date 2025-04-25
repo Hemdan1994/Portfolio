4ce703882c4277c35afa1a946c256ff4ed2c8aa2
Mohamed Hemdan <hemdan@drjobs.ae>
2025-04-25 05:59:38 +0300

    Updates

--- src/components/Home/ProfileImage.tsx
+++ src/components/Home/ProfileImage.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { cn } from '@/lib/utils';
 
 const ProfileImage = ({ className }: { className?: string }) => {
+  const profileImageUrl = new URL('/../src/assets/images/personal.jpg', import.meta.url).href;
   return (
     <div className={cn("relative group", className)}>
       <div className="relative rounded-2xl overflow-hidden bg-highlight/20 z-10">
         <div className="absolute inset-0 bg-gradient-to-tr from-highlight/20 to-transparent group-hover:opacity-0 transition-opacity duration-300 z-20"></div>
         <img 
-          src="./../../../src/assets/images/personal.jpg" 
+          src={profileImageUrl} 
           alt="Profile" 
           className="w-full aspect-square object-cover rounded-2xl grayscale group-hover:grayscale-0 transition-all duration-300 hover:scale-105"
         />
